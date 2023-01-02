fd76f96d72f3d9a63c64c10d2491614966984e4c
Sandeep Soni <sandeepsoni@berkeley.edu>
2023-01-02 05:07:24 -0800

    update the shell script with spatial relation prediction

---
+++ shell_scripts/spatial_relation_train_and_eval.sh
@@ -0,0 +1,1 @@
+python ../scripts/spatial_relation_train_and_eval.py --pretrained-model-name bert-base-cased --training-data-file ../data/annotations/final_annotations/final_annotations.v1.pickle --num-epochs 10 --context-field context_100 --model-path ../data/experiments/spatial_prediction/model_checkpoints/trained.v1.pt --num-labels 9
