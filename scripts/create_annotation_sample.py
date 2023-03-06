7fcf64723c156977e49339b4f0cd5d87f3d06739
Sandeep Soni <sandeepsoni@berkeley.edu>
2023-03-06 09:04:48 +0000

    create the annotation sample that contains one more sentence

--- scripts/create_annotation_sample.py
+++ scripts/create_annotation_sample.py
@@ -1,3 +1,9 @@
+"""
+
+Usage:
+Author: Sandeep Soni
+"""
+
 import argparse
 import os
 import glob
@@ -7,6 +13,7 @@ from string import ascii_uppercase
 from functools import reduce
 from tqdm import tqdm
 import random
+import sys
 
 class ReservoirSampler ():
     def __init__ (self, k):
@@ -49,17 +56,22 @@ def readArgs ():
     args = parser.parse_args ()
     return args
 
-def main (args):
-    rs = ReservoirSampler (args.sample_size)
-    for filename in tqdm (glob.glob (os.path.join (args.collocation_dir, "*.collocations"))):
+def gen_random_examples (collocation_dir, num_samples=10000, sep="\t"):
+    rs = ReservoirSampler (num_samples)
+    for filename in tqdm (glob.glob (os.path.join (collocation_dir, "*.collocations"))):
         with open (filename) as fin:
             for i, line in enumerate (fin):
                 if i == 0:
-                    header = line.strip().split ("\t")
+                    header = line.strip().split (sep)
                 if i > 0:
                     rs.add (line)
 
     ids = [gen_random_string () for item in rs.reservoir]
+
+    return rs, ids, header
+
+def main (args):
+    rs, ids, header = gen_random_examples (args.collocation_dir, num_samples=args.sample_size)
 
     rows = list ()
     for i, item in enumerate (rs.reservoir):
@@ -74,6 +86,10 @@ def main (args):
         book_id = row["book_id"]
         start = min(int (row["persons_start_token"]), int (row["locations_start_token"]))
         end = max(int (row["persons_end_token"]), int(row["locations_end_token"])) 
+        special_token_positions = {int (row["persons_start_token"]), 
+                                   int (row["persons_end_token"]),
+                                   int (row["locations_start_token"]),
+                                   int (row["locations_end_token"])}
 
         for context_window in args.context_window_sizes:
             from_here = start - context_window
@@ -84,17 +100,38 @@ def main (args):
                 if os.path.exists(path) and os.path.getsize(path) > 0:
                     break
 
+            target_sent_id = sys.maxsize
             with open (path) as fin:
                 context = list ()
                 for j, line in enumerate (fin):
                     if j == 0:
                         continue
                     parts = line.strip().split ("\t")
-                    token_id = int (parts[3])
+                    sent_id = int (parts[1]) # get the sentence ID
+                    token_id = int (parts[3]) # get the token ID
                     if token_id < from_here:
                         continue
                     elif token_id >= from_here and token_id <= till_there:
-                        context.append (parts[4])
+                        if token_id not in special_token_positions:
+                            if sent_id <= (target_sent_id+1): # Consider the sentence after the sentence that contained the last mention
+                                context.append (parts[4]) # append the token
+                            else:
+                                break
+                        else:
+                            if token_id == int (row["persons_start_token"]):
+                                context.append ("<char>")
+                            if token_id == int (row["locations_start_token"]):
+                                context.append ("<place>")
+
+                            context.append (parts[4])
+                            
+                            if token_id == int (row["persons_end_token"]):
+                                context.append ("</char>")
+                            if token_id == int (row["locations_end_token"]):
+                                context.append ("</place>")
+
+                            if token_id == end:
+                                target_sent_id = int (parts[1])
                     else:
                         break
 
@@ -103,9 +140,25 @@ def main (args):
         output.append (row)
 
     output_df = pd.DataFrame (output)
-    output_df["is_character_correct"] = ""
-    output_df["is_location_correct"] = ""
-    output_df["is_character_at_location"] = ""
+    # rename some columns
+    output_df = output_df.rename (columns={"persons_start_token": "char_start_token", 
+                                           "persons_end_token": "char_end_token", 
+                                           "persons_text": "char_text",
+                                           "persons_coref": "char_coref",
+                                           "persons_tag": "char_tag",
+                                           "persons_cat": "char_cat",
+                                           "locations_start_token": "place_start_token",
+                                           "locations_end_token": "place_end_token",
+                                           "locations_text": "place_text",
+                                           "locations_coref": "place_coref",
+                                           "locations_tag": "place_tag",
+                                           "locations_cat": "place_cat"})
+
+    output_df["valid_relation"] = ""
+    output_df["spatial_relation"] = ""
+    output_df["temporal_span"] = ""
+    output_df["narrative_tense"] = ""
+    output_df["comments"] = ""
     output_df.to_csv (args.output_file, sep="\t", index=False, header=True)
 
 if __name__ == "__main__":
