45c33dacf593c74bc14396d10834f72c2b7e895b
Sandeep Soni <soni.sandeepb@gmail.com>
2023-03-20 15:18:45 -0700

    lightly refactor the code for inferential gender extraction from file

--- scripts/gender_inference.py
+++ scripts/gender_inference.py
@@ -1,3 +1,5 @@
+""" Extract the inferential gender for the characters in each book."""
+
 import argparse
 import os
 import glob
@@ -12,10 +14,19 @@ def readArgs ():
     args = parser.parse_args ()
     return args
 
-def main (args):
+
+def read_inferred_genders_from_file (dirnames, cols=[]):
+    """ Read the inferred genders from the file.
+
+    Arguments:
+    dirnames(list): A list of the directory names that contain individual book files.
+    cols(list): A list of column names (str)
+    """
+
     rows = list ()
-    rows.append (["book_id", "char_id", "inf_gender", "prob"])
-    for dirname in args.booknlp_dirs:
+    rows.append (cols)
+
+    for dirname in dirnames:
         for filename in tqdm (glob.glob (os.path.join (dirname, f"*.book"))):
             book_id = os.path.basename (filename).split (".")[0]
             with open (filename) as fin:
@@ -27,8 +38,12 @@ def main (args):
                                   char["id"], \
                                   char["g"]["argmax"], \
                                   char["g"]["max"]])
-
+                    
     df = pd.DataFrame (rows[1:], columns=rows[0])
+    return df
+
+def main (args):
+    df = read_inferred_genders_from_file (args.booknlp_dirs, cols=["book_id", "char_id", "inf_gender", "prob"])
     df.to_csv (args.output_file, sep="\t", header=True, index=False)
 
 if __name__ == "__main__":
