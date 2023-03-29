4fb6c31c097332226cf7ccbef0194b161a3e9b5f
Sandeep Soni <soni.sandeepb@gmail.com>
2023-03-29 01:02:54 -0700

    correct the indentation

--- scripts/valid_relation_train_and_eval.py
+++ scripts/valid_relation_train_and_eval.py
@@ -31,35 +31,35 @@ def preprocess_valid_relation_prediction (annotations, *args, **kwargs):
     return full_df, train_df, test_df
 
 def readArgs ():
-	parser = argparse.ArgumentParser (description="Script to train and evaluate a spatial relation prediction model")
-	parser.add_argument ("--pretrained-model-name", required=True, type=str, help="Name of the pretrained model")
-	parser.add_argument ("--annotated-data-file", required=True, type=str, help="Annotated data is in this pickle file")
-	parser.add_argument ("--test-ids-file", required=False, default="", type=str, required="Test file contains IDS on which we want to test")
-	parser.add_argument ("--training-frac", required=False, default=0.8, type=float, help="Training fraction")
-	parser.add_argument ("--num-epochs", required=False, default=10, type=int, help="Number of epochs for training")
-	parser.add_argument ("--context-field", required=False, default="context_100", type=str, help="Column name that contains the entire text")
-	parser.add_argument ("--model-path", required=True, type=str, help="Path to the file that will store the model")
-	parser.add_argument ("--num-labels", required=False, type=int, default=2, help="The number of labels in the spatial relation prediction task")
-	args = parser.parse_args ()
-	return args
+    parser = argparse.ArgumentParser (description="Script to train and evaluate a spatial relation prediction model")
+    parser.add_argument ("--pretrained-model-name", required=True, type=str, help="Name of the pretrained model")
+    parser.add_argument ("--annotated-data-file", required=True, type=str, help="Annotated data is in this pickle file")
+    parser.add_argument ("--test-ids-file", required=False, default="", type=str, required="Test file contains IDS on which we want to test")
+    parser.add_argument ("--training-frac", required=False, default=0.8, type=float, help="Training fraction")
+    parser.add_argument ("--num-epochs", required=False, default=10, type=int, help="Number of epochs for training")
+    parser.add_argument ("--context-field", required=False, default="context_100", type=str, help="Column name that contains the entire text")
+    parser.add_argument ("--model-path", required=True, type=str, help="Path to the file that will store the model")
+    parser.add_argument ("--num-labels", required=False, type=int, default=2, help="The number of labels in the spatial relation prediction task")
+    args = parser.parse_args ()
+    return args
 
 def main (args):
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-	predictor = BERTRelationPrediction (model_name=args.pretrained_model_name,
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    predictor = BERTRelationPrediction (model_name=args.pretrained_model_name,
                                         dims=768,
-					                    n_labels=args.num_labels,
-					                    device=device,
-					                    lr=1e-6)
-	predictor.load_data (args.annotated_data_file, 
+                                        n_labels=args.num_labels,
+                                        device=device,
+                                        lr=1e-6)
+    predictor.load_data (args.annotated_data_file, 
                          preprocess=preprocess_valid_relation_prediction, 
                          test_ids_file=args.test_ids_file, 
                          training_frac=args.training_frac)
 	
-	predictor.start_training (num_epochs=args.num_epochs, 
+    predictor.start_training (num_epochs=args.num_epochs, 
                               context_field=args.context_field, 
                               verbose=True)
 	
-	predictor.save_model (args.model_path)
+    predictor.save_model (args.model_path)
 
 if __name__ == "__main__":
-	main (readArgs ())
+    main (readArgs ())
