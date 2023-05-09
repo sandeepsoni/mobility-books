7b578ca661c7fbd7c09cc026a1c0206d94a94bd6
Sandeep Soni <soni.sandeepb@gmail.com>
2023-05-09 00:05:33 -0700

    update script

--- scripts/get_sentence_end_token_indices.py
+++ scripts/get_sentence_end_token_indices.py
@@ -33,7 +33,7 @@ def modify_context (paths, row):
     filename = correct_path (paths, f"{book_id}.tokens")
     df = read_file (filename, sep='\t')
     df = df.query ('token_ID_within_document == @last_token')
-    sent_id = df['sentence_ID']
+    sent_id = df['sentence_ID'].values[0]
     return sent_id
 
 def main (args):
