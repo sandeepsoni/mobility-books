31b4c029fca6444acde8879a4487d7acf6d602e9
Sandeep Soni <soni.sandeepb@gmail.com>
2023-05-21 11:07:19 -0700

    the final setting for context

--- shell_scripts/relation_prediction_wrapper.sh
+++ shell_scripts/relation_prediction_wrapper.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-for context_size in context_50; do
+for context_size in context_100; do
     for task_name in validity spatial temporal_span narrative_tense; do
         /bin/bash relation_prediction.sh $task_name $context_size 1754 0;
         /bin/bash relation_prediction.sh $task_name $context_size 1754 1;
