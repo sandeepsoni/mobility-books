9c2b6cc7dbcf0b99861bc6f5512efcfbdf2ca678
Sandeep Soni <soni.sandeepb@gmail.com>
2023-05-21 07:42:51 -0700

    update the relations wrapper script

--- shell_scripts/relation_prediction_wrapper.sh
+++ shell_scripts/relation_prediction_wrapper.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
-
-/bin/bash relation_prediction.sh validity context_10 1754 0
-/bin/bash relation_prediction.sh validity context_10 1754 1
-/bin/bash relation_prediction.sh spatial context_10 1754 0
-/bin/bash relation_prediction.sh spatial context_10 1754 1
-/bin/bash relation_prediction.sh temporal_span context_10 1754 0
-/bin/bash relation_prediction.sh temporal_span context_10 1754 1
-/bin/bash relation_prediction.sh narrative_tense context_10 1754 0
-/bin/bash relation_prediction.sh narrative_tense context_10 1754 1
+for context_size in context_50; do
+    for task_name in validity spatial temporal_span narrative_tense; do
+        /bin/bash relation_prediction.sh $task_name $context_size 1754 0;
+        /bin/bash relation_prediction.sh $task_name $context_size 1754 1;
+    done
+done
 
