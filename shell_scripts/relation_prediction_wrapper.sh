0807374fa2f3a2c48f7289218bac2949233241c6
Sandeep Soni <soni.sandeepb@gmail.com>
2023-05-21 04:45:10 +0000

    update the relation prediction wrapper script

--- shell_scripts/relation_prediction_wrapper.sh
+++ shell_scripts/relation_prediction_wrapper.sh
@@ -2,3 +2,10 @@
 
 /bin/bash relation_prediction.sh validity context_10 1754 0
 /bin/bash relation_prediction.sh validity context_10 1754 1
+/bin/bash relation_prediction.sh spatial context_10 1754 0
+/bin/bash relation_prediction.sh spatial context_10 1754 1
+/bin/bash relation_prediction.sh temporal_span context_10 1754 0
+/bin/bash relation_prediction.sh temporal_span context_10 1754 1
+/bin/bash relation_prediction.sh narrative_tense context_10 1754 0
+/bin/bash relation_prediction.sh narrative_tense context_10 1754 1
+
