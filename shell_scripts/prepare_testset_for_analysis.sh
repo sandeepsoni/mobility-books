16f95ccee084cefb51c87497e3c07f38435c5433
Sandeep Soni <soni.sandeepb@gmail.com>
2023-05-22 22:47:39 -0700

    add the shell script to execute the collocations and examples

---
+++ shell_scripts/prepare_testset_for_analysis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run on redwood
+parallel --jobs 16 \
+         python ../scripts/prepare_testset.py \
+         --book-id {} \
+         --collocations-dir ../data/v3 \
+         --content-dir /data0/ssoni/booknlp/contemporary_litbank \
+         --context-window-size 100 \
+         --output-dir ../data/experiments/testsets/contemporary_litbank/collocations
+         ::: $(cat ../data/experiments/testsets/contemporary_litbank/contemporary_litbank.ids)
