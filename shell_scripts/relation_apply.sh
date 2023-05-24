87b555024e7f8cdcd905f6366162039b980c62bf
Sandeep Soni <soni.sandeepb@gmail.com>
2023-05-24 03:55:13 -0700

    minor change

--- shell_scripts/relation_apply.sh
+++ shell_scripts/relation_apply.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 python ../scripts/relation_apply.py --validity-model-path ../data/models/bert_models/context_100/0/1754/validity.pt \
-                                    --book-ids $(head -n 5 ../data/experiments/testsets/contemporary_litbank/contemporary_litbank.ids) \
+                                    --book-ids $(cat $1) \
                                     --collocations-dir ../data/experiments/testsets/contemporary_litbank/collocations \
                                     --output-dir ../data/experiments/testsets/contemporary_litbank/predictions
