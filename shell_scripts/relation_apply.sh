39253dad5c2d314ffdc163e973da0852b98a01e7
Sandeep Soni <soni.sandeepb@gmail.com>
2023-05-24 10:00:48 +0000

    fix typo

--- shell_scripts/relation_apply.sh
+++ shell_scripts/relation_apply.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-python ../scripts/relation_apply.py --validity-model-path ..data/models/bert_models/context_100/0/1754/validity.pt \
+python ../scripts/relation_apply.py --validity-model-path ../data/models/bert_models/context_100/0/1754/validity.pt \
                                     --book-ids $(head -n 5 ../data/experiments/testsets/contemporary_litbank/contemporary_litbank.ids) \
-                                    --collocation-dirs ../data/experiments/testsets/contemporary_litbank/collocations \
+                                    --collocations-dir ../data/experiments/testsets/contemporary_litbank/collocations \
                                     --output-dir ../data/experiments/testsets/contemporary_litbank/predictions
