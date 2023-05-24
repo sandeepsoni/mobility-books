64a9ad964b19ae58fb565164895a0899a5f7e5dd
Sandeep Soni <soni.sandeepb@gmail.com>
2023-05-24 04:15:22 -0700

    apply all the models

--- scripts/relation_apply.py
+++ scripts/relation_apply.py
@@ -60,30 +60,48 @@ def main (args):
     validity_model.load_state_dict (validity_checkpoint["model_state_dict"])
 
     # load the spatial model
-    #spatial_checkpoint = torch.load (args.spatial_model_path)
-    #spatial_model = BERTRelationPrediction (n_labels=config_options["spatial"]["num_labels"],
-    #                                        labels=config_options["spatial"]["label_space"])
-    #spatial_model.load_state_dict (spatial_checkpoint["model_state_dict"])
+    spatial_checkpoint = torch.load (args.spatial_model_path)
+    spatial_model = BERTRelationPrediction (n_labels=config_options["spatial"]["num_labels"],
+                                            labels=config_options["spatial"]["label_space"],
+                                            device=device)
+    spatial_model.load_state_dict (spatial_checkpoint["model_state_dict"])
 
     # load the temporal span model
-    #temporal_span_checkpoint = torch.load (args.temporal_span_model_path)
-    #temporal_span_model = BERTRelationPrediction (n_labels=config_options["temporal_span"]["num_labels"],
-    #                                              labels=config_options["temporal_span"]["label_space"])
-    #temporal_span_model.load_state_dict (temporal_span_checkpoint["model_state_dict"])
+    temporal_span_checkpoint = torch.load (args.temporal_span_model_path)
+    temporal_span_model = BERTRelationPrediction (n_labels=config_options["temporal_span"]["num_labels"],
+                                                  labels=config_options["temporal_span"]["label_space"],
+                                                  device=device)
+    temporal_span_model.load_state_dict (temporal_span_checkpoint["model_state_dict"])
 
     # load the narrative tense model
-    #narrative_tense_checkpoint = torch.load (args.narrative_tense_model_path)
-    #narrative_tense_model = BERTRelationPrediction (n_labels=config_options["temporal_span"]["num_labels"],
-    #                                                labels=config_options["temporal_span"]["label_space"])
-    #narrative_tense_model.load_state_dict (narrative_tense_checkpoint["model_state_dict"])
+    narrative_tense_checkpoint = torch.load (args.narrative_tense_model_path)
+    narrative_tense_model = BERTRelationPrediction (n_labels=config_options["narrative_tense"]["num_labels"],
+                                                    labels=config_options["narrative_tense"]["label_space"],
+                                                    device=device)
+    narrative_tense_model.load_state_dict (narrative_tense_checkpoint["model_state_dict"])
         
     for book_id in args.book_ids:
         book_df = pd.read_csv (os.path.join (args.collocations_dir, f"{book_id}.examples"), sep="\t")
         validity_predictions = validity_model.apply_book (book_df,
                                                           text_field=args.text_field)
-        predictions = [VALID_LABELS[prediction] for prediction in validity_predictions]
-        book_df["validity"] = predictions
-        #book_df = book_df[book_df["binary_classifier_predictions"] == "VALID"]
+        validity_predictions = [VALID_LABELS[prediction] for prediction in validity_predictions]
+        book_df["validity"] = validity_predictions
+        
+        spatial_predictions = spatial_model.apply_book (book_df,
+                                                        text_field=args.text_field)
+        spatial_predictions = [SPATIAL_LABELS[prediction] for prediction in spatial_predictions]
+        book_df["spatial"] = spatial_predictions
+
+        temporal_span_predictions = temporal_span_model.apply_book (book_df,
+                                                                    text_field=args.text_field)
+        temporal_span_predictions = [TEMPORAL_SPAN_LABELS[prediction] for prediction in temporal_span_predictions]
+        book_df["temporal_span"] = temporal_span_predictions
+
+        narrative_tense_predictions = narrative_tense_model.apply_book (book_df,
+                                                                        text_field=args.text_field)
+        narrative_tense_predictions = [NARRATIVE_TENSE_LABELS[prediction] for prediction in narrative_tense_predictions]
+        book_df["narrative_tense"] = narrative_tense_predictions
+        
         os.makedirs (args.output_dir, exist_ok=True)
         book_df.to_csv (os.path.join (args.output_dir, f"{book_id}.predictions"), sep="\t", header=True, index=False)
 
