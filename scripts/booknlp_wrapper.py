f99e44c21670dd798a6ee732d2adc1f04b1962c6
Sandeep Soni <soni.sandeepb@gmail.com>
2023-06-16 19:01:00 -0700

    Modify script to generate output only when output files are not present

--- scripts/booknlp_wrapper.py
+++ scripts/booknlp_wrapper.py
@@ -11,6 +11,17 @@ parser.add_argument ("--bookids-file", type=str, required=True, help="File conta
 parser.add_argument ("--output-dir", type=str, required=True, help="Output directory will create the files")
 args = parser.parse_args ()
 
+def output_files_exist (output_dir, book_id):
+    return all ([
+        os.path.exists (os.path.join (output_dir, f"{book_id}.book")),
+        os.path.exists (os.path.join (output_dir, f"{book_id}.book.html")),
+        os.path.exists (os.path.join (output_dir, f"{book_id}.tokens")),
+        os.path.exists (os.path.join (output_dir, f"{book_id}.entities")),
+        os.path.exists (os.path.join (output_dir, f"{book_id}.quotes")),
+        os.path.exists (os.path.join (output_dir, f"{book_id}.supersense"))
+
+    ])
+
 # Read the book_ids from file
 with open (args.bookids_file) as fin:
     book_ids = fin.read().splitlines ()
@@ -27,4 +38,6 @@ booknlp=BookNLP("en", model_params)
 
 for book_id in tqdm (book_ids):
     input_filename = os.path.join (args.input_dir, f"{book_id}.txt")
+    if output_files_exist (args.output_dir, book_id):
+        continue
     booknlp.process(input_filename, args.output_dir, book_id)
