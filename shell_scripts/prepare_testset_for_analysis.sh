d1876fcc07fd3298ed13b44f45eafda7d649b2d5
Sandeep Soni <soni.sandeepb@gmail.com>
2023-06-21 15:34:52 -0700

    update the prep testset script

--- shell_scripts/prepare_testset_for_analysis.sh
+++ shell_scripts/prepare_testset_for_analysis.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 # Run on redwood
+#parallel --jobs 16 \
+#         python ../scripts/prepare_testset.py \
+#         --book-id {} \
+#         --collocations-dir ../data/v3 \
+#         --content-dir /data0/ssoni/booknlp/contemporary_litbank \
+#         --context-window-size 100 \
+#         --output-dir ../data/experiments/testsets/contemporary_litbank/collocations \
+#         ::: $(cat ../data/experiments/testsets/contemporary_litbank/contemporary_litbank.ids)
+
 parallel --jobs 16 \
          python ../scripts/prepare_testset.py \
          --book-id {} \
-         --collocations-dir ../data/v3 \
-         --content-dir /data0/ssoni/booknlp/contemporary_litbank \
+         --collocations-dir ../data/experiments/testsets/conlit/collocations \
+         --content-dir ../data/experiments/testsets/conlit/booknlp \
          --context-window-size 100 \
-         --output-dir ../data/experiments/testsets/contemporary_litbank/collocations \
-         ::: $(cat ../data/experiments/testsets/contemporary_litbank/contemporary_litbank.ids)
+         --output-dir ../data/experiments/testsets/conlit/examples \
+         :::: ../data/experiments/testsets/conlit/conlit.ids
