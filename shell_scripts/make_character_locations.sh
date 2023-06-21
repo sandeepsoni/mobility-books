2752a71f0aa9736d9fca8ab9403dcfcc34e631f4
Sandeep Soni <soni.sandeepb@gmail.com>
2023-06-21 13:01:52 -0700

    add comments

--- shell_scripts/make_character_locations.sh
+++ shell_scripts/make_character_locations.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-OUTPUT_DIR=$1
-IDS_FILE=$2
+OUTPUT_DIR=$1 # The BookNLP directory.
+IDS_FILE=$2 # IDS in a single file
 
 #parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {} --output-dir ../data/v2 --window-size 10 ::: $(ls /mnt/data0/kentkchang/charemotions/corpus/booknlp.1.0.7/gutenberg_fiction_tagged_1_4/*.entities)
 #parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {} --output-dir ../data/v2 --window-size 10 ::: $(ls /mnt/data0/kentkchang/charemotions/corpus/booknlp.1.0.7/gutenberg_fiction_tagged_5_9/*.entities)
