885ea10b299820b14a3c22304580d01cc3937163
Sandeep Soni <soni.sandeepb@gmail.com>
2023-06-21 13:06:25 -0700

    minor path fixes

--- shell_scripts/make_character_locations.sh
+++ shell_scripts/make_character_locations.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-OUTPUT_DIR=$1 # The BookNLP directory.
-IDS_FILE=$2 # IDS in a single file
+INPUT_DIR=$1    # The BookNLP directory.
+OUTPUT_DIR=$2   # The Collocations directory.
+IDS_FILE=$3     # IDS in a single file
 
 #parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {} --output-dir ../data/v2 --window-size 10 ::: $(ls /mnt/data0/kentkchang/charemotions/corpus/booknlp.1.0.7/gutenberg_fiction_tagged_1_4/*.entities)
 #parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {} --output-dir ../data/v2 --window-size 10 ::: $(ls /mnt/data0/kentkchang/charemotions/corpus/booknlp.1.0.7/gutenberg_fiction_tagged_5_9/*.entities)
 
-parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {}.entities --output-dir $OUTPUT_DIR --window-size 10 :::: $IDS_FILE
+parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename $INPUT_DIR/{}.entities --output-dir $OUTPUT_DIR --window-size 10 :::: $IDS_FILE
