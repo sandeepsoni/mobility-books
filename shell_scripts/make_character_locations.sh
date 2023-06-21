ba672fb552e096e0b34c2838a45896480ca05c8b
Sandeep Soni <soni.sandeepb@gmail.com>
2023-06-21 12:59:45 -0700

    add more general gnu parallel command

--- shell_scripts/make_character_locations.sh
+++ shell_scripts/make_character_locations.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
-parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {} --output-dir ../data/v2 --window-size 10 ::: $(ls /mnt/data0/kentkchang/charemotions/corpus/booknlp.1.0.7/gutenberg_fiction_tagged_1_4/*.entities)
-parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {} --output-dir ../data/v2 --window-size 10 ::: $(ls /mnt/data0/kentkchang/charemotions/corpus/booknlp.1.0.7/gutenberg_fiction_tagged_5_9/*.entities)
+OUTPUT_DIR=$1
+IDS_FILE=$2
+
+#parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {} --output-dir ../data/v2 --window-size 10 ::: $(ls /mnt/data0/kentkchang/charemotions/corpus/booknlp.1.0.7/gutenberg_fiction_tagged_1_4/*.entities)
+#parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {} --output-dir ../data/v2 --window-size 10 ::: $(ls /mnt/data0/kentkchang/charemotions/corpus/booknlp.1.0.7/gutenberg_fiction_tagged_5_9/*.entities)
+
+parallel --jobs 16 python ../scripts/name_location_cooccurrences.py --input-filename {}.entities --output-dir $OUTPUT_DIR --window-size 10 :::: $IDS_FILE
