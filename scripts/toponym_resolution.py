a68bc15b75800fc30f82e9e34167d077aa983d4b
Sandeep Soni <soni.sandeepb@gmail.com>
2023-06-23 02:44:14 -0700

    first pass toponym resolution

--- scripts/toponym_resolution.py
+++ scripts/toponym_resolution.py
@@ -3,6 +3,8 @@ import argparse
 import os, glob
 from tqdm import tqdm
 import json
+import unidecode
+import re
 
 def readArgs ():
     parser = argparse.ArgumentParser (description="Extract all toponym candidates and then do resolution")
@@ -17,9 +19,28 @@ def get_entities_from_book (filename):
     df = pd.read_csv (filename, sep="\t", on_bad_lines='skip', engine="python")
     return df
 
+def format_into_query (toponym_candidate):
+    # remove accents
+    toponym_candidate = unidecode.unidecode(toponym_candidate) #remove accents
+    toponym_candidate = toponym_candidate.lower() # lowercase
+    toponym_candidate = re.sub('[^0-9a-zA-Z]+', ' ', toponym_candidate) # replace non-alphanumerics by space
+    toponym_candidate = ' '.join(toponym_candidate.split()) # replace multiple whitespaces with a single whitespace
+    return toponym_candidate
+
+def create_json (book_id, coref_id, start_token, end_token, text, query, num_matches):
+    js = {}
+    js["book_id"] = book_id
+    js["coref_id"] = coref_id
+    js["start_token"]= start_token
+    js["end_token"] = end_token
+    js["text"] = text
+    js["query"] = query
+    js["num_matches"] = num_matches
+    return js
+
+
 def main (args):
     # Read all the locations from the entities file
-    """
     all_locs = list ()
     for filename in tqdm (glob.glob (os.path.join(args.booknlp_dir, "*.entities"))):
         entities_df = get_entities_from_book (filename)
@@ -27,14 +48,25 @@ def main (args):
         entities_df["book_id"] = book_id
         locs_df = entities_df.query ('cat == @args.cat')
         all_locs.append (locs_df)
+        break
 
     all_locs = pd.concat (all_locs)
-    print (len (all_locs))
-    """
+    all_locs["query_text"] = all_locs["text"].apply (lambda x: format_into_query (x))
+
     # Read all entries from the gazeteer
     gaz = pd.read_csv (args.gazetteer_file, sep='\t', quoting=True, quotechar='"')
+    gaz = gaz.query ('lang == "en"')
+    gaz = gaz[["text_string", "place_id", "lat", "lon"]]
     print (len (gaz))
     # Output a JSON files
+    with open (args.output_json_file, "w") as fout:
+        for i, loc in all_locs.iterrows ():
+            q = loc["query_text"]
+            df = gaz.query ('text_string == @q')
+            num_matches = len (df)
+            js = create_json (loc["book_id"], loc["COREF"], loc["start_token"], loc["end_token"], loc["text"], q, num_matches)
+            fout.write (f"{json.dumps (js)}\n")
+
 
 if __name__ == "__main__":
     main (readArgs ())
